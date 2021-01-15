a39ac130c0a965223f849a7146c8552c161b127e
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-15 14:18:40 +0100

    Definito costruttore nella classe Movie

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
         public $title;
         public $year;
         public $director;
+
+        function __construct($_title, $_year, $_director) {
+            $this->title = $_title;
+            $this->year = $_year;
+            $this->director = $_director;
+        }
     };
 
-    $myMovie = new Movie();
-    $myMovie->title = 'Star Wars';
-    var_dump($myMovie);
-    echo $myMovie->title;
+
 ?>
