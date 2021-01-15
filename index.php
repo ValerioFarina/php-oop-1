1931da6790ae27339d423b6d045e60319aa1f8e9
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-15 14:27:10 +0100

    Inserito un getter e un setter per ogni variabile di istanza

--- index.php
+++ index.php
@@ -9,7 +9,36 @@
             $this->year = $_year;
             $this->director = $_director;
         }
+
+        public function getTitle() {
+            return $this->title;
+        }
+
+        public function setTitle($new_title) {
+            $this->title = $new_title;
+        }
+
+        public function getYear() {
+            return $this->year;
+        }
+
+        public function setYear($new_year) {
+            $this->year = $new_year;
+        }
+
+        public function getDirector() {
+            return $this->director;
+        }
+
+        public function setDirector($new_director) {
+            $this->director = $new_director;
+        }
     };
+
+    $myMovie = new Movie('Star Wars', 1977, 'George Lucas');
+    echo $myMovie->getTitle();
+    $myMovie->setTitle('Star Wars - Episode IV');
+    var_dump($myMovie);
 
 
 ?>
