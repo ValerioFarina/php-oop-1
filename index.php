b5244d38fb5fd2ec6705bb2e1f877b62a5827a48
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-15 14:13:35 +0100

    Creata una classe Movie con 3 variabili di istanza

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-    echo 'hello';
+    class Movie {
+        public $title;
+        public $year;
+        public $director;
+    };
+
+    $myMovie = new Movie();
+    $myMovie->title = 'Star Wars';
+    var_dump($myMovie);
+    echo $myMovie->title;
 ?>
